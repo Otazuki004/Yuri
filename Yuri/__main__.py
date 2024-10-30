7145a3e9bec1a9aa603d82ac31cd5b766f35b4ab
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-10-30 23:59:30 +0530

    Update __main__.py

--- Yuri/__main__.py
+++ Yuri/__main__.py
@@ -7,4 +7,4 @@ async def on_ready():
 	await ch.send('I woke up...')
 
 if __name__ == '__main__':
-  Yuri.run('MTMwMDc3MzE1NTc0ODQ0NjIyOA.Gvslhf.Tak40dbpTFbfL1pAPJ-xdoxi1eEmQWVRsY1gQs')
+  Yuri.run(TOKEN)
