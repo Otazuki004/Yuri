e221a9a86c2108fa40151e399a8ac9a7356074fb
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-10-30 23:44:02 +0530

    Update __main__.py

--- Yuri/__main__.py
+++ Yuri/__main__.py
@@ -1,11 +1,10 @@
 from discord import *
 from Yuri import *
-from Yuri.__init__ import Yuri
 
 @Yuri.event
 async def on_ready():
 	ch = Yuri.get_channel(1301179913885323334)
 	await ch.send('I woke up...')
 
-if name == '__main__':
+if __name__ == '__main__':
   Yuri.run("MTMwMDc3MzE1NTc0ODQ0NjIyOA.G3-FOg.2lD4_Ce4h_RRowM2kvbaa4W-Gisqf15GAbVYk8")
