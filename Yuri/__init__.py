2dcf9d393ba2737c5da12089bd2197d1df64dd0d
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-10-30 23:33:31 +0530

    Update __init__.py

--- Yuri/__init__.py
+++ Yuri/__init__.py
@@ -1,1 +1,14 @@
-print('Hellowww')
+from discord import *
+from discord.ext import *
+import logging
+
+logging.basicConfig(
+    format="[Yuri] %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
+
+intents = Intents.default()
+intents.message_content = True 
+
+Yuri = commands.Bot(command_prefix="!", intents=intents)
