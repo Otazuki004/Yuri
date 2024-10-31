c828c6744221bc47c0378c29ae6d79597fa3f6c9
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-10-31 13:58:27 +0530

    Create logs.py

---
+++ Yuri/functions/logs.py
@@ -0,0 +1,15 @@
+from subprocess import getoutput as r
+from discord import File, Embed
+from discord.ext import commands
+from Yuri.decorator.filter_user import filter_user
+
+@filter_user('otazuki')
+async def log_cmd(ctx, *args, **kwargs):
+    stats = await ctx.reply('Loading...')
+    log = r('tail log.txt')
+    await stats.delete()
+    embed = Embed(title='Log output', description=f"```python\n{log}```", color='0xFF0000')
+    await ctx.reply(embed=embed)
+
+async def setup(Yuri):
+    Yuri.add_command(commands.Command(log_cmd, name=['log', 'logs']))
