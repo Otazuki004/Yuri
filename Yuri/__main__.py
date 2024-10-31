256df60ac244251fc29b7dbdb5db65c68bfed43d
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-10-31 12:04:54 +0530

    Update __main__.py

--- Yuri/__main__.py
+++ Yuri/__main__.py
@@ -1,10 +1,15 @@
 from discord import *
 from Yuri import *
+import asyncio
 
 @Yuri.event
 async def on_ready():
 	ch = Yuri.get_channel(1301179913885323334)
 	await ch.send('I woke up...')
 
+async def start():
+	await load_mods()
+	await Yuri.run(TOKEN)
+	
 if __name__ == '__main__':
-  Yuri.run(TOKEN)
+	asyncio.run(start())
