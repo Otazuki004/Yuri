c06c39bbde94edbe7f31e879a244d6d3e0a094b3
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-10-31 12:58:24 +0530

    Update filter_user.py

--- Yuri/decorator/filter_user.py
+++ Yuri/decorator/filter_user.py
@@ -1,16 +1,22 @@
-def filter_user(user, reject_him=False):
+def filter_user(*users, reject_him=False):
     def decorator(func):
         async def wrapper(ctx, *args, **kwargs):
-            if user.isdigit():
-                if ctx.author.id != int(user) and reject_him == False:
-                    return await ctx.send("You do not have permission to use this command.")
-                elif reject_him == True and ctx.author.id == int(user):
-                    return await ctx.send("You do not have permission to use this command.")
-            else:
-                if ctx.author.name != user and reject_him == False:
-                    return await ctx.send("You do not have permission to use this command.")
-                elif reject_him == True and ctx.author.name == user:
-                    return await ctx.send("You do not have permission to use this command.")
+            match_found = False
+            for user in users:
+                if isinstance(user, int):
+                    if ctx.author.id == user:
+                        match_found = True
+                        if reject_him:
+                            return await ctx.send("You do not have permission to use this command.")
+                elif isinstance(user, str):
+                    if ctx.author.name == user:
+                        match_found = True
+                        if reject_him:
+                            return await ctx.send("You do not have permission to use this command.")
+            if not match_found and not reject_him:
+                return await ctx.send("You do not have permission to use this command.")
+
             return await func(ctx, *args, **kwargs)
+
         return wrapper
     return decorator
